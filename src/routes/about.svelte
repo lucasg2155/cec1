2cea36acd2b9766d2d8a88a80f73220807f66874
lucasg2155 <ggcon.gabriel@gmail.com>
2020-02-04 02:00:28 -0300

    cambio en page mision

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -2,6 +2,6 @@
 	<title>About</title>
 </svelte:head>
 
-<h1>About this site</h1>
+<h1>Nuestra Misión</h1>
 
-<p>This is the 'about' page. There's not much here.</p>
+<p>Con casi 50 años de trayectoria somos la primera Universidad no estatal de la Comunidad Valenciana. Una Universidad de servicio público y gestión privada que desde su origen se ha caracterizado por su carácter pionero, por ser los primeros en ofrecer en Valencia estudios universitarios como farmacia, periodismo o veterinaria. Por ser los primeros en ofrecer dobles titulaciones. Por ser los primeros en integrar la figura del tutor en la universidad. Por ser los primeros en ofrecer becas de desarrollo profesional durante la carrera. Por ser los primeros en poner en marcha otra forma de entender qué es una Universidad.</p>
--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -21,7 +21,7 @@
 	<title>Blog</title>
 </svelte:head>
 
-<h1>Recent posts</h1>
+<h1>Últimas Noticias</h1>
 
 <ul>
 	{#each posts as post}
