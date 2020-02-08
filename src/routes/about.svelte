5ef7f769b920bb367e21c20449e1a9da7fd0b5ff
lucasg2155 <ggcon.gabriel@gmail.com>
2020-02-08 17:05:40 -0300

    cambio mision img

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -4,4 +4,9 @@
 
 <h1>Nuestra Misión</h1>
 
-<p>El Sindicato de Empleados de Comercio de Ushuaia, es una organización sindical, sin fines lucrativos ni políticos, cuya misión es cumplir y hacer cumplir los derechos y obligaciones de las y los trabajadores comercio establecidos en el Contrato Colectivo de Trabajo vigente; implementar programas de formación, capacitación y actualización que permitan a nuestros agremiados ser altamente competitivos y estar a la vanguardia en el crecimiento para el reconocimiento nacional, así como reforzar las actividades sociales, culturales, deportivas y los valores universales que permitan mejorar la calidad laboral y personal de cada uno de nuestros trabajadores.</p>
+<p>El Sindicato de Empleados de Comercio de Ushuaia, es una organización sindical, sin fines lucrativos ni políticos, cuya misión es cumplir y hacer cumplir los derechos y obligaciones de las y los trabajadores de comercio establecidos en el Contrato Colectivo de Trabajo vigente; implementar programas de formación, capacitación y actualización que permitan a nuestros agremiados ser altamente competitivos y estar a la vanguardia en el crecimiento para el reconocimiento nacional, así como reforzar las actividades sociales, culturales, deportivas y los valores universales que permitan mejorar la calidad laboral y personal de cada uno de nuestros trabajadores.</p>
+
+<figure>
+	<img alt='logo' src='logocecu.jpg'>
+	<figcaption>CEC Ushuaia</figcaption>
+</figure>
