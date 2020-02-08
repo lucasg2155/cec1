306356cc63909a1387233d539e39e851f2ec2b25
lucasg2155 <ggcon.gabriel@gmail.com>
2020-02-08 16:17:21 -0300

    cambio mision

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -4,4 +4,4 @@
 
 <h1>Nuestra Misión</h1>
 
-<p>Con casi 25 años de trayectoria somos el primer gremio no estatal de la Comunidad. Una gremio de servicio y gestión que desde su origen se ha caracterizado por su carácter pionero, por ser los primeros en ofrecer farmacia, clinica y odontologia. Por ser los primeros en ofrecer dobles beneficios. Por ser de los primeros en integrar la figura del tutor en la obra social. Por ser los primeros en ofrecer becas de desarrollo profesional durante la carrera. Por ser los primeros en poner en marcha otra forma de entender qué es ser un trabajador.</p>
+<p>Con casi 25 años de trayectoria somos el primer gremio no estatal de la Comunidad.</p>
