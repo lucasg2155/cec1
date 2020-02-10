07167d482ac168697feed5c55d1527c466f6f07c
lucasg2155 <ggcon.gabriel@gmail.com>
2020-02-10 15:58:16 -0300

    agregar mapa4

--- src/components/Map.svelte
+++ src/components/Map.svelte
@@ -1,26 +1,27 @@
+
 <script>
+	//import mapStyles from './map-styles'; // optional
 
- src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBDaeWicvigtP9xPv919E-RNoxfvC-Hqik&callback=iniciarMap";
-
- function iniciarMap(){
-    var coord = {lat:-54.8065109 ,lng: -68.3141208};
-    var map = new google.maps.Map(document.getElementById('map'),{
-      zoom: 10,
-      center: coord
+	let container;
+	let map;
+	let zoom = 8;
+    let center = {lat: -54.8065109, lng: -68.3141208};
+    
+    import { onMount } from 'svelte';
+    
+	onMount(async () => {
+		map = new google.maps.Map(container, {
+            zoom,
+			center,
+			//styles: mapStyles // optional
+		});
     });
-    var marker = new google.maps.Marker({
-      position: coord,
-      map: map
-    });
-}
-
-	 export let map;
 </script>
-
 <style>
-    map{
-	height: 500px;
-	width: 100%;
-    }
+.full-screen {
+    width: 100vw;
+    height: 100vh;
+}
 </style>
 
+<div class="full-screen" bind:this={container}></div>
