efb39b8b9f74bb818edf51c7e9923b6a30f5a424
lucasg2155 <ggcon.gabriel@gmail.com>
2020-02-10 17:38:16 -0300

    agregar mapa cecu2

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,16 +1,5 @@
-<!-- <script>
-	import Map from '../components/Map.svelte';
-	export let ready
-
-	src="https://maps.googleapis.com/maps/api/js?key=&callback=initMap";
-
-</script>; -->
-
 <style>
 
-    /* :global(body) {
-	padding: 0
-	} */
 	h1, figure, p {
 		text-align: center;
 		margin: 0 auto;
@@ -24,6 +13,12 @@
 	}
 
 	figure {
+		margin: 0 0 1em 0;
+	}
+	iframe {
+		margin: 0 0 1em 0;
+		width: 100%;
+		max-width: 400px;
 		margin: 0 0 1em 0;
 	}
 
@@ -58,10 +53,6 @@
 	<img alt='cecu' src='cecu.jpg'>
 	<figcaption>Sede Sindical</figcaption>
 </figure>
-
-<!-- { #if ready }
-<Map></Map>
-{ /if } -->
 
 <figure>
 <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1149.7357982553297!2d-68.3147282!3d-54.8068293!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xbc4c23218a5e80b9%3A0x943a7c699e6ea0c7!2sCECU!5e0!3m2!1ses!2sar!4v1581365683721!5m2!1ses!2sar" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
