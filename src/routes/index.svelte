9f8a2f2353d87d28495713b7fba478b2d5c05ab7
lucasg2155 <ggcon.gabriel@gmail.com>
2020-02-10 17:20:05 -0300

    agregar mapa cecu1

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -62,6 +62,10 @@
 <!-- { #if ready }
 <Map></Map>
 { /if } -->
+
+<figure>
 <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1149.7357982553297!2d-68.3147282!3d-54.8068293!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xbc4c23218a5e80b9%3A0x943a7c699e6ea0c7!2sCECU!5e0!3m2!1ses!2sar!4v1581365683721!5m2!1ses!2sar" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+<figcaption>Gobernador Paz 1081</figcaption>
+</figure>
 
 <p><strong> Derechos Reservados © 2020 creado por: <a  title = "ggcon"  href = "http://ggcon.com.ar">ggcon</a></strong></p>
